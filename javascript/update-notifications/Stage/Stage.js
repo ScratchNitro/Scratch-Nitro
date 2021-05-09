23a3b12a63274f58ebd39ed58a4285ff6478bf99
JackyBorderCollie <67914267+JackyBorderCollie@users.noreply.github.com>
2021-05-09 10:54:34 +0200

    Add files via upload

---
+++ javascript/update-notifications/Stage/Stage.js
@@ -0,0 +1,29 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Stage as StageBase,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Stage extends StageBase {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("backdrop1", "./Stage/costumes/backdrop1.svg", {
+        x: 240,
+        y: 180
+      })
+    ];
+
+    this.sounds = [new Sound("pop", "./Stage/sounds/pop.wav")];
+
+    this.triggers = [];
+
+    this.vars.shown = "true";
+  }
+}
